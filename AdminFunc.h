e22090af2d1b6fcd0cc308fcd7d422cbfa32da2a
CometNurella <vominhtien2008@yahoo.com>
2025-07-27 13:06:26 +0000

    Add includes, function prototypes

--- AdminFunc.h
+++ AdminFunc.h
@@ -1,6 +1,70 @@
+#include<stdio.h>
+#include<stdlib.h>
+#include<iostream>
+#include<sstream>
+#include<fstream>
+#include<string>
+#include<iomanip>
+#include<algorithm>
+#include<deque>
+
+using namespace std;
+
+
+void UpdateData();
+void AddStudentDetails();
+void DeleteStudent();
+void ViewTable();
+void ReturnToMainMenu();
+void Exit();
+
+void AdminMenu() {
+    cout << "\n\t\t|  Logged in as admin  |\n";
+    cout << "\n\t\t1. Add student details\n";
+    cout << "\n\t\t2. Delete student\n";
+    cout << "\n\t\t3. View table\n";
+    cout << "\n\t\t4. Return to main menu\n";
+    cout << "\n\t\t5. Exit\n";
+    cout << "\n\t\tEnter choice : ";
+}
+
+
 void Admin_Function() {
-    //This function will have its own header file
-    cout << "\n\tAdmin Function\n";
+    bool BooleanFlag {false};
+    int choice;
+    
+    // cout << "\n\tAdmin Function\n";
+
+    do {
+        AdminMenu();
+        cin >> choice;
+
+        switch (choice) {
+            case 1: AddStudentDetails();
+            break;
+
+            case 2: DeleteStudent();
+            break;
+
+            case 3: ViewTable();
+            break;
+
+            case 4: ReturnToMainMenu();
+            break;
+
+            case 5: BooleanFlag = true;
+                cout << "\n\t\tExit Admin Menu\n";   
+            break;
+
+            default: cout << "\t\tInvalid option\n";
+
+        }
+    } while (!BooleanFlag);
+
+
+
+
+    
 
 
 }
