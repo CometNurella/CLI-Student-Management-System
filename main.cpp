e5c78ff2341c05f1969f7dc10622b18d2cf19640
CometNurella <vominhtien2008@yahoo.com>
2025-07-27 10:48:33 +0000

    Pre-Prototype

--- main.cpp
+++ main.cpp
@@ -2,6 +2,8 @@
 #include<iostream>
 #include<sstream>
 #include<fstream>
+#include<string>
+#include<iomanip>
 #include<algorithm>
 #include<deque>
 
@@ -9,7 +11,12 @@
 using namespace std;
 
 void Home_Display() {
-    
+    cout << "\t\tLogin as :\n";
+    cout << "\n\t\t1. Admin\n";
+    cout << "\n\t\t2. Student\n";
+    cout << "\n\t\t3. Exit\n";
+    cout << "\n\n\t\tEnter your choice : ";
+
 
 
     
@@ -17,6 +24,27 @@ void Home_Display() {
 
 int main() {
     // cout << "Hello World\n";
+    // Home_Display();
+
+    bool BooleanFlag {true};
+    int option;
+
+
+    do {
+        switch (option) {
+        case '1':
+        break;
+
+        case '2':
+        break; 
+
+        case '3': BooleanFlag = false;
+        break;
+
+        default: cout << "Invalid option\n";
+        }
+    } while (BooleanFlag);
+    
 
     return 0;
 }
