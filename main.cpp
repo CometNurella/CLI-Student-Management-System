36be1ff3e1fdf774631a3ad19ac0c3122e224936
CometNurella <vominhtien2008@yahoo.com>
2025-07-27 11:20:45 +0000

    Prototype

--- main.cpp
+++ main.cpp
@@ -10,8 +10,8 @@
 
 using namespace std;
 
-void Home_Display() {
-    cout << "\t\tLogin as :\n";
+void MainMenu() {
+    cout << "\n\t\tLogin as :\n";
     cout << "\n\t\t1. Admin\n";
     cout << "\n\t\t2. Student\n";
     cout << "\n\t\t3. Exit\n";
@@ -22,28 +22,47 @@ void Home_Display() {
     
 }
 
+void Admin_Function() {
+    //This function will have its own header file
+    cout << "\n\tAdmin Function\n";
+
+
+}
+
+void Student_Function() {
+    //This will use a function from Admin_Function()
+    cout << "\n\tStudent Function\n";
+
+
+}
+
 int main() {
     // cout << "Hello World\n";
-    // Home_Display();
+    // MainMenu();
 
-    bool BooleanFlag {true};
-    int option;
+    bool BooleanFlag {false};
+    int choice;
+    // cin >> choice;
 
 
     do {
-        switch (option) {
-        case '1':
+        MainMenu();
+        cin >> choice;
+
+        switch (choice) {
+        case 1: Admin_Function();
         break;
 
-        case '2':
+        case 2: Student_Function();
         break; 
 
-        case '3': BooleanFlag = false;
+        case 3: BooleanFlag = true;
+            cout << "\n\t\tExit program\n";
         break;
 
         default: cout << "Invalid option\n";
         }
-    } while (BooleanFlag);
+    } while (!BooleanFlag);
     
 
     return 0;
