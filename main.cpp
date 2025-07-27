df73b60ae10dd30fc3ae11b22850ecab3d339575
CometNurella <vominhtien2008@yahoo.com>
2025-07-27 14:50:57 +0000

    Clean up main.cpp

--- main.cpp
+++ main.cpp
@@ -14,17 +14,6 @@ void MainMenu() {
 
 }
 
-// void Admin_Function() {
-//     //This function will have its own header file
-//     cout << "\n\tAdmin Function\n";
-// }
-
-
-// void Student_Function() {
-//     //This will use a function from Admin_Function()
-//     cout << "\n\tStudent Function\n";
-// }
-
 int main() {
     // cout << "Hello World\n";
 
