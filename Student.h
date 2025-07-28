6df9ccc2c2902d6e3cbe9280c18f1a2a69f0f6e3
CometNurella <vominhtien2008@yahoo.com>
2025-07-28 01:37:35 +0000

    Change type of some member variables to string for getline compatability

--- Student.h
+++ Student.h
@@ -16,18 +16,16 @@ class Student {
         return os;
     }
 private:
-    // static int nextID;
     int ID;
-    std::string NAME{"-"}, COLLEGE{"-"}, SUBJECT{"-"};
-    int ATTENDANCE{0}, PHYSICS{0}, CHEMISTRY{0}, MATHS{0};
+    std::string NAME{"-"}, COLLEGE{"-"}, SUBJECT{"-"}, ATTENDANCE{"-"}, PHYSICS{"-"}, CHEMISTRY{"-"}, MATHS{"-"};
 public:
     Student()
         : ID{0} {}
 
     Student(std::string name, std::string college, std::string subject,
-            int attendance, int physics, int chemistry, int maths)
+            std::string attendance, std::string physics, std::string chemistry, std::string maths)
         : ID{0}, NAME{std::move(name)}, COLLEGE{std::move(college)}, SUBJECT{std::move(subject)},
-          ATTENDANCE{attendance}, PHYSICS{physics}, CHEMISTRY{chemistry}, MATHS{maths} {}
+          ATTENDANCE{std::move(attendance)}, PHYSICS{std::move(physics)}, CHEMISTRY{std::move(chemistry)}, MATHS{std::move(maths)} {}
 
     ~Student() = default;
 
@@ -43,13 +41,10 @@ public:
     const std::string& getName() const { return NAME; }
     const std::string& getCollege() const { return COLLEGE; }
     const std::string& getSubject() const { return SUBJECT; }
-    int getAttendance() const { return ATTENDANCE; }
-    int getPhysics() const { return PHYSICS; }
-    int getChemistry() const { return CHEMISTRY; }
-    int getMaths() const { return MATHS; }
+    const std::string& getAttendance() const { return ATTENDANCE; }
+    const std::string& getPhysics() const { return PHYSICS; }
+    const std::string& getChemistry() const { return CHEMISTRY; }
+    const std::string& getMaths() const { return MATHS; }
 };
-
-// Definition of static member
-   
 
 #endif // _STUDENT_H_
